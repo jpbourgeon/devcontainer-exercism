999369ad074ffd512322801ed7a54b46d6334734
jpbourgeon <3356641+jpbourgeon@users.noreply.github.com>
2021-11-05 16:16:52 +0000

    add exercism to the path

--- .devcontainer/post-attach.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-cd /home/node
-
-## init exercism
-echo 'export PATH=~/bin:$PATH' >> ~/.bash_profile
-source ~/.bash_profile
-cd /workspaces/exercises
--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,11 +7,16 @@ cd /home/node
 wget -nc https://github.com/cli/cli/releases/download/v2.2.0/gh_2.2.0_linux_amd64.deb
 dpkg -i gh_2.2.0_linux_amd64.deb
 
-## Setup Exercism workspace folder
+## Exercism 3.0.13
 mkdir -p /home/node/exercism
 cd /home/node/exercism
 wget -nc https://github.com/exercism/cli/releases/download/v3.0.13/exercism-3.0.13-linux-x86_64.tar.gz
 tar -xf exercism-3.0.13-linux-x86_64.tar.gz
 mkdir -p ~/bin
 mv exercism ~/bin
-mkdir /workspaces/exercises
+echo "" >> ~/.bashrc
+echo "# Exercism" >> ~/.bashrc
+echo "export PATH=~/bin:$PATH" >> ~/.bashrc
+
+## Exercices folder
+mkdir -p /workspaces/exercises
